680ffdfc21df359136ec348479a2b4f8f7e1331b
Sam Van Kooten <vankooten.sam@gmail.com>
2020-07-22 18:13:46 -0600

    Improve firestore2local utility

--- utils/firestore2local.py
+++ utils/firestore2local.py
@@ -1,7 +1,9 @@
 # Since cache entries in Firestore are stored as opaque, compressed blobs,
 # this script will download a blob, decompress it, and save the data as JSON
 # to a file.
-# Syntax: python firestore2local.py [author|doc] [cache key] [filename]
+# Syntax: python firestore2local.py <author|doc> <cache key> [filename]
+# If no filename is given, the cache key is used
+# Tip: view the file contents as `cat <filename> | python -m json.tool`
 
 import json
 import sys
@@ -27,4 +29,9 @@ except CacheMiss:
     print("Second arg must be a valid cache key")
     sys.exit()
 
-json.dump(result, open(sys.argv[3], 'w'))
+if len(sys.argv) > 3:
+    fname = sys.argv[3]
+else:
+    fname = sys.argv[2]
+
+json.dump(result, open(fname, 'w'))
